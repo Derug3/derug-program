0493fd0bda58dc31f856b7a5f092dc987cb1aba0
Nikola Drašković <draskovic26z@gmail.com>
2023-02-25 22:57:51 +0100

    fixes cancel_derug ix

--- programs/derug-program/src/instructions/cancel_derug_request.rs
+++ programs/derug-program/src/instructions/cancel_derug_request.rs
@@ -18,27 +18,17 @@ pub struct CancelDerugRequest<'info> {
 }
 
 pub fn cancel_derug_request(ctx: Context<CancelDerugRequest>) -> Result<()> {
-    let derug_data = &mut ctx.accounts.derug_data;
-    derug_data.total_suggestion_count = derug_data.total_suggestion_count.checked_sub(1).unwrap();
-
     require!(
         ctx.accounts.derug_request.derugger.key() == ctx.accounts.payer.key(),
         DerugError::WrongDerugger
     );
 
-    let derug_request_info = ctx.accounts.derug_request.to_account_info();
+    let derug_data = &mut ctx.accounts.derug_data;
+    derug_data.total_suggestion_count = derug_data.total_suggestion_count.checked_sub(1).unwrap();
 
-    **ctx.accounts.payer.lamports.borrow_mut() = ctx
-        .accounts
-        .payer
-        .lamports()
-        .checked_add(derug_request_info.lamports())
-        .unwrap();
-
-    **derug_request_info.lamports.borrow_mut() = 0;
-
-    let mut derug_request_data = derug_request_info.data.borrow_mut();
-    derug_request_data.fill(0);
+    ctx.accounts
+        .derug_request
+        .close(ctx.accounts.payer.to_account_info())?;
 
     Ok(())
 }
